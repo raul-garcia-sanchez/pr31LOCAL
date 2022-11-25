6d5b3beffd39cdcced53bd3a80f5b4372b9b0566
raul-garcia-sanchez <raul-garcia-sanchez>
2022-11-25 16:59:46 +0100

    add exercises with PDO

---
+++ src/index.php
@@ -0,0 +1,56 @@
+<html>
+ <head>
+ 	<title>Exercici SQL COUNTRIES</title>
+ 	<style>
+ 		body{
+ 		}
+ 		table,td {
+ 			border: 1px solid black;
+ 			border-spacing: 0px;
+ 		}
+ 	</style>
+ </head>
+ 
+ <body>
+ 	<h1>Filtre de ciutats per país</h1>
+ 
+ 	<?php
+
+	try{
+		$hostname = "127.0.0.1";
+		$dbname = "world";
+		$username = "admin";
+		$pw = "admin123";
+		$pdo = new PDO ("mysql:host=$hostname;dbname=$dbname","$username","$pw");
+	} catch (PDOException $e) {
+		echo "Failed to get DB handle: " . $e->getMessage() . "\n";
+		exit;
+	}
+
+	$query = $pdo->prepare('SELECT * FROM country;');
+	$query->execute();
+ 	?>
+ 
+    <form action="results.php" method="POST">
+ 	<label for="codi_pais">Choose a country:</label>
+ 	<select name="codi_pais" id="codi_pais">
+
+ 	<?php
+
+		$row = $query->fetch();
+ 		
+ 		while( $row)
+ 		{
+            echo "<option value=".$row["Code"].">".$row["Name"]."</option>";
+			$row = $query->fetch();
+ 		}
+		
+		unset($pdo);
+		unset($query);
+ 	?>
+
+    </select>
+    <input type="submit" name="" id="">
+    </form>
+ </body>
+</html>
---
+++ src/results.php
@@ -0,0 +1,57 @@
+<html>
+ <head>
+ 	<h1>Filtre de ciutats per país</h1>
+ 	<style>
+ 		body{
+ 		}
+ 		table,td {
+ 			border: 1px solid black;
+ 			border-spacing: 0px;
+ 		}
+ 	</style>
+ </head>
+ 
+ <body>
+ 	<h1>Exemple de lectura de dades a MySQL</h1>
+ 
+ 	<?php
+
+		try{
+			$hostname = "127.0.0.1";
+			$dbname = "world";
+			$username = "admin";
+			$pw = "admin123";
+			$pdo = new PDO ("mysql:host=$hostname;dbname=$dbname","$username","$pw");
+		} catch (PDOException $e) {
+			echo "Failed to get DB handle: " . $e->getMessage() . "\n";
+			exit;
+		}
+
+		$query = $pdo->prepare("SELECT c.Name as NameCity, co.Name as NamePais FROM city c inner join country co on c.CountryCode = co.Code where co.Code ='".$_POST["codi_pais"]."';");
+		$query->execute();
+
+ 	?>
+ 
+ 	<table>
+ 	
+ 	<thead><td colspan="4" align="center" bgcolor="cyan">Llistat de ciutats</td></thead>
+ 	<?php
+
+		$row = $query->fetch();
+ 		
+ 		while( $row )
+ 		{
+ 			echo "\t<tr>\n";
+ 			echo "\t\t<td>".$row["NameCity"]."</td>\n";
+ 			echo "\t\t<td>".$row['NamePais']."</td>\n";
+ 			echo "\t</tr>\n";
+			 $row = $query->fetch();
+ 		}
+
+		unset($pdo); 
+		unset($query);
+ 	?>
+
+ 	</table>	
+ </body>
+</html>
